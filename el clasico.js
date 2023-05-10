dabb5e2415fc65eebae363f6fa8c27d0bae03ebf
sar-v-esh <133137461+sar-v-esh@users.noreply.github.com>
2023-05-10 21:11:32 +0530

    Add files via upload

---
+++ el clasico.js
@@ -0,0 +1,10 @@
+function validate()
+{
+    var a= document.getElementById("n").value;
+    var b= document.getElementById("pass").value;
+
+    if (a=="user" && b=="pass"){
+        alert("Thanks")
+    }
+    
+}
